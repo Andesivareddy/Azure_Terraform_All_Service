e3891caaff60962f2fe702848ae43ff1c50031f7
Ande <sande@estee.com>
2024-02-27 16:11:36 +0530

    Create Azure DevOps Using Terraform

---
+++ Terraform-All_Services/All-Services-Files/Deploy-Terraform-for-AzureDevOps/Terraform-code/main.tf
@@ -0,0 +1,20 @@
+
+
+#Sample Code
+
+
+terraform {
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+ # version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "resourcegroup-test-tbd-3"
+  location = "westeurope"
+}
---
+++ Terraform-All_Services/All-Services-Files/Deploy-Terraform-for-AzureDevOps/readme.md
@@ -0,0 +1,2 @@
+This demo shows how to deploy infrastructure into Azure using Terraform and Azure DevOps Yaml pipelines.
+
